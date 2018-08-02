23bca29803f372c2de305ba3aa367c7a05b399aa
Chahinaz <chahinaz@dindane.com>
2018-08-02 18:09:50 +0200

    [WIP] adding Recipe as a vue class, and call it in our index

---
+++ components/Recipe.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="recipes">
+    <article class="recipe">
+      <div class="thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
+      <h1>Title</h1>
+      <p>Some usefull text</p>
+    </article>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: ["thumbnail", "title", "previewText"]
+  }
+</script>
+
+<style scope>
+  .recipes {
+    display: flex;
+    flex-flow: row wrap; /*responsive style*/
+    justify-content: center; /*flew box property*/
+    align-items: center; /*flew box property*/
+  }
+
+  .recipe{
+    box-sizing: border-box;
+    width: 280px;
+    padding: 8px;
+    border: 1px solid #fff;
+    box-shadow: 0 2px 2px #aaa;
+    margin: 10px;
+  }
+
+  .thumbnail {
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+  }
+</style>
--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,46 +1,27 @@
 <template>
   <section class="recipes">
-    <article class="recipe">
-      <div class="thumbnail" style="background-image: url(https://cac.img.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fplat.2Fhamburgers.2Fburger-maison-283827.2F3698362-1-fre-FR.2Fburger-maison.2Ejpg/1200x600/crop-from/center/burger-maison.jpg)"></div>
-        <h1>Title 1</h1>
-        <p>Some usefull text</p>
-    </article>
+    <Recipe
+      thumbnail="https://cac.img.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fplat.2Fhamburgers.2Fburger-maison-283827.2F3698362-1-fre-FR.2Fburger-maison.2Ejpg/1200x600/crop-from/center/burger-maison.jpg"
+      title="Homemade burger."
+      previewText="Delicious homemade burger."></Recipe>
 
-    <article class="recipe">
-      <div class="thumbnail" style="background-image: url(https://www.atelierdeschefs.com/media/recette-e17282-burger-au-cheddar-potatoes-au-melange-cajun.jpg)"></div>
-        <h1>Title 2</h1>
-        <p>Still, that's usefull text</p>
-    </article>
+    <Recipe
+      thumbnail="http://akispetretzikis.com/system/uploads/medium/data/8404/akis-petretzikis-burger.jpg"
+      title="Homemade burgers."
+      previewText="Super fat homemade burgers."></Recipe>
   </section>
 </template>
 
 
 <script>
+  import Recipe from '@/components/Recipe.vue'
 
+  export default {
+    components: {
+      Recipe
+    }
+  }
 </script>
 
 
-<style scoped>
-  .recipes {
-    display: flex;
-    flex-flow: row wrap; /*responsive style*/
-    justify-content: center; /*flew box property*/
-    align-items: center; /*flew box property*/
-  }
 
-  .recipe{
-    box-sizing: border-box;
-    width: 280px;
-    padding: 8px;
-    border: 1px solid #fff;
-    box-shadow: 0 2px 2px #aaa;
-    margin: 10px;
-  }
-
-  .thumbnail {
-    background-position: center;
-    background-size: cover;
-    width: 100%;
-    height: 200px;
-  }
-</style>
