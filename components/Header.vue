b5e2f9608eb25057256fa4c370768fb959b02d2e
Chahinaz <chahinaz@dindane.com>
2018-08-03 11:30:10 +0200

    [COMPLETE] adding a header to navigate between home page and recipes page

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Staring Page</nuxt-link></li>
+        <li><nuxt-link to="/recipes">Recipes</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<style>
+  header {
+    width: 100%;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  li {
+    margin: 0 10px;
+  }
+</style>
